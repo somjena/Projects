8fd07e82e27a37c5390b855ae97937d791e87311
somjena <jenasoumitrapvt@gmail.com>
2025-03-02 23:49:39 +0530

    Employee Driver Class

--- JDBC/EmployeeManagement/src/main/java/org/EmpManagement/EmpDriver/EmployeeDriver.java
+++ JDBC/EmployeeManagement/src/main/java/org/EmpManagement/EmpDriver/EmployeeDriver.java
@@ -1,4 +1,35 @@
 package org.EmpManagement.EmpDriver;
+import org.EmpManagement.EmpRepo.EmpRepository;
+import org.EmpManagement.EmpService.EmpService;
+
+import java.util.Scanner;
 
 public class EmployeeDriver {
+    public static void main(String[] args) {
+        System.out.println("     "+"===========================================");
+        System.out.println("               Welcome to Employee Management      ");
+        System.out.println("     "+"===========================================");
+        EmpRepository emp = new EmpService();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("====================Enter Choice=======================");
+        while(true){
+            System.out.println("1.Add Employee \n2.Update Details\n3.Delete Employee \n4.Fetch Employee\n5.Exit");
+            int choice=sc.nextInt();
+            switch (choice){
+                case 1:emp.registerEmp();
+                break;
+                case 2:emp.updateEmp();
+                break;
+                case 3:emp.deleteEmp();
+                break;
+                case 4:emp.empDetails();
+                break;
+                case 5:emp.exit();
+                return;
+                default:
+                    System.out.println("Enter the Wrong input");
+
+            }
+        }
+    }
 }
