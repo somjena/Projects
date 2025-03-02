49a459955ec3d7a4c90fa546155b162df654016a
somjena <jenasoumitrapvt@gmail.com>
2025-03-02 23:50:17 +0530

    Employeee Class

--- JDBC/EmployeeManagement/src/main/java/org/EmpManagement/Entity/Emp.java
+++ JDBC/EmployeeManagement/src/main/java/org/EmpManagement/Entity/Emp.java
@@ -1,8 +1,41 @@
 package org.EmpManagement.Entity;
 
 public class Emp {
-    int id;
-    String name;
-    int age;
-    double sal;
+     private int id;
+   private String name;
+    private int age;
+   private double sal;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSal() {
+        return sal;
+    }
+
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
 }
