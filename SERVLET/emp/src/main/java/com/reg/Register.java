afb6cfe062747ce64309dc4f8801e419204fae69
somjena <jenasoumitrapvt@gmail.com>
2025-03-21 23:10:05 +0530

    Update

--- SERVLET/emp/src/main/java/com/reg/Register.java
+++ SERVLET/emp/src/main/java/com/reg/Register.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,12 +17,11 @@ public class Register extends HttpServlet{
 		String name  = req.getParameter("name");
 		String age = req.getParameter("age");
 		String email = req.getParameter("email");
-		PrintWriter pr = resp.getWriter();
-		pr.print("<html><body>");
-		pr.print("<h1> Student name is :-"+name+"</h1>");
-		pr.print("<h1> Student age is :-"+age+"</h1>");
-		pr.print("<h1> Student email is :-"+email+"</h1>");
-		pr.print("</body></html>");
+		Cookie ck1= new Cookie("name", name);
+		Cookie ck2= new Cookie("email", email);
+		resp.addCookie(ck1);
+		resp.addCookie(ck2);
+		resp.getWriter().print("Cookies Created");
 		
 		
 		
